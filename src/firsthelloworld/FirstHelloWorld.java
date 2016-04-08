dd19ffda27fd1fd61563543d9b83211a43839ba1
g13942sy <g13942sy@s30449.ACTSUDA>
2016-04-08 12:00:19 +0900

    See you, Worldって出力を追加しました

--- src/firsthelloworld/FirstHelloWorld.java
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -16,7 +16,7 @@ public class FirstHelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        
+        System.out.println("See you, World");
         // TODO code application logic here
     }
     
